42d3870d1eb9c703b61675b1ea4674cbd4a2b877
DoTheMoses <matthewkiep@gmail.com>
2016-03-10 03:13:32 -0500

    Initial commit of files
    Initial commit of files

---
+++ js/players_melee.js
@@ -0,0 +1,27 @@
+var player_5 = {
+        name: 		"Moses"
+};
+var player_4 = {
+        name: 		"Colbol"
+};
+var player_3 = {
+        name:		"BluEG"
+};
+var player_2 = {
+        name: 		"Harriet"
+};      
+var player_1 = {
+        name: 		"Plup"
+};
+var player_0 = {
+        name: 		"Fearless"
+};
+
+var players_melee = [
+	player_5,
+	player_4,
+	player_3, 
+	player_2, 
+	player_1, 
+	player_0 
+];
---
+++ js/tournaments_melee.js
@@ -0,0 +1,93 @@
+var tournament_8 = {
+        date: 		"11/16/15",
+	event_link: 	"https://www.facebook.com/events/778555268937803/",
+	event_name: 	"CFL Smackdown 54: Melee Singles ft. Melee Doubles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"null",
+	vods_link:	"https://www.youtube.com/playlist?list=PLWqagSqx492fI6Cmnzo3W5yO3PgE5bRf3",
+};
+var tournament_7 = {
+        date: 		"11/9/15",
+	event_link: 	"https://www.facebook.com/events/1490073807988588/",
+	event_name: 	"CFL Smackdown 53: Melee Singles ft. PM Singles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-53-11-09-15-melee-singles.9863/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u4-9sVN0NU8NrmB9CZjBKHc8",
+};
+var tournament_6 = {
+        date: 		"11/2/15",
+	event_link: 	"https://www.facebook.com/events/1011216762256873/",
+	event_name: 	"CFL Smackdown 52: One Year of Smackdowns ft. Melee Singles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-52-11-02-15-melee-singles.9754/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u49ckTd3SFTGLaxGP7kgDvqF",
+};
+var tournament_5 = {
+        date: 		"10/19/15",
+	event_link: 	"https://www.facebook.com/events/1488802518115534/",
+	event_name: 	"CFL Smackdown 51: Melee Singles ft. Melee Doubles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-51-10-19-15-melee-singles.9509/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u4_CfVAkRL4_YpSfmKK-qQfW",
+};      
+var tournament_4 = {
+        date: 		"10/12/15",
+	event_link: 	"https://www.facebook.com/events/194423977555328/",
+	event_name: 	"CFL Smackdown 50: Melee Singles ft. PM Singles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-50-10-12-15-melee-singles.9384/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u4-8CqJ22Sl0XE0PcLzMWd9l",
+};
+var tournament_3 = {
+        date: 		"10/5/15",
+	event_link: 	"https://www.facebook.com/events/948260855232678/",
+	event_name: 	"CFL Smackdown 49: Melee Singles ft. Melee Doubles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-49-10-05-15-melee-singles.9227/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u48SGS2WBh-ZTfG3T8bpnBwJ",
+};
+var tournament_2 = {
+        date: 		"9/28/15",
+	event_link: 	"https://www.facebook.com/events/960154000713003/",
+	event_name: 	"CFL Smackdown 48: Melee Singles ft. PM Singles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-48-9-28-15-melee-singles.9084/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u48kd5T37GUSG7QOHCNfu2yP",
+};
+var tournament_1 = {
+        date: 		"9/21/15",
+	event_link: 	"https://www.facebook.com/events/143372892676769/",
+	event_name: 	"CFL Smackdown 47: Melee Singles ft. Melee Doubles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-47-9-21-15-melee-singles.8950/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u4-WdjkjWWodLnOGYlaPuUpi",
+};
+var tournament_0 = {
+        date: 		"9/14/15",
+	event_link: 	"https://www.facebook.com/events/955773274464171/",
+	event_name: 	"CFL Smackdown 46: Melee Singles",
+	location_link: 	"http://www.campuscardsandgames.com/",
+	location:	"Campus Cards & Games",
+	results_link:	"http://smashboards.com/rankings/cfl-smackdown-weekly-46-9-14-15-melee-singles.8795/event",
+	vods_link:	"https://www.youtube.com/playlist?list=PLqSexIuL7u48vbKdQW5UyKTmx_dQhJtRB",
+};
+
+var tournaments_melee = [
+tournament_8, 
+tournament_7, 
+tournament_6, 
+tournament_5, 
+tournament_4, 
+tournament_3, 
+tournament_2, 
+tournament_1, 
+tournament_0
+];
