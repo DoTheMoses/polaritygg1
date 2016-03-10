3fc9e67f7ed6fc23898ee4b6a2e366b48a6bd272
DoTheMoses <matthewkiep@gmail.com>
2016-03-10 03:17:03 -0500

    Added test folder

---
+++ Test/table_info.js
@@ -0,0 +1,30 @@
+var tournament_1 = {
+	link: 		"yay",
+	results:	"another!",
+        date: 		"Monday",
+	first: 		"Plup",
+	second: 	"gats",
+	third: 		"blat"
+};
+var tournament_2 = {
+	link: 		"pit",
+	results:	"time",
+        date: 		"Tuesday",
+	first: 		"Gahtzu",
+	second: 	"EG",
+	third: 		"OG"
+};
+var tournament_3 = {
+	link: 		"snow",
+	results:	"ball",
+        date: 		"Wednesday",
+	first: 		"notail",
+	second: 	"VP",
+	third: 		"Lil"
+};
+      
+var tournaments = [
+tournament_1, 
+tournament_2, 
+tournament_3
+];
